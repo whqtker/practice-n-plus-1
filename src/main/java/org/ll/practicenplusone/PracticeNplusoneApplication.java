c4129dcf6c9143e0b20b0449c8db58cb8dd24185
seonghyeok <chosh2001@naver.com>
2025-07-06 15:48:45 +0900

    init

---
+++ src/main/java/org/ll/practicenplusone/PracticeNplusoneApplication.java
@@ -0,0 +1,13 @@
+package org.ll.practicenplusone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeNplusoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeNplusoneApplication.class, args);
+    }
+
+}
