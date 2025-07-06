00d3c8dcbaca60753007d1e05ef02323ff821307
seonghyeok <chosh2001@naver.com>
2025-07-06 15:56:04 +0900

    feat: 엔티티 생성

---
+++ src/main/java/org/ll/practicenplusone/comment/entity/Comment.java
@@ -0,0 +1,25 @@
+package org.ll.practicenplusone.comment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import org.ll.practicenplusone.post.entity.Post;
+
+@Entity
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    private Post post;
+}
---
+++ src/main/java/org/ll/practicenplusone/post/entity/Post.java
@@ -0,0 +1,30 @@
+package org.ll.practicenplusone.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import org.ll.practicenplusone.comment.entity.Comment;
+
+import java.util.List;
+
+@Entity
+@Getter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+}
